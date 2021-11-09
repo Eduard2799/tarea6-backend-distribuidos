5bdd7112b5f9c87cf41420cdfc7c35ef9de6d052
Eduard2799 <55165801+Eduard2799@users.noreply.github.com>
2021-11-09 10:59:35 -0600

    Update headersCORS.js

--- netlify/functions/headersCORS.js
+++ netlify/functions/headersCORS.js
@@ -4,6 +4,8 @@
 
 module.exports =  {
   'Access-Control-Allow-Origin': '*',
-  'Access-Control-Allow-Headers': 'content-type',
-  'Access-Control-Allow-Methods': 'POST, GET, PUT, DELETE, OPTIONS'
+  'Access-Control-Allow-Methods': 'POST, GET, PUT, DELETE, OPTIONS',
+  'Access-Control-Allow-Headers': 'X-Requested-With,content-type',
+  'Access-Control-Allow-Credentials': true,
+  'content-Type' : 'application/json'
 };
